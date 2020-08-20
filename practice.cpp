ead5249987a25e159d5e6cd0f7253f538d53bf69
PriyadharshanRavi <mailmefriends95@gmail.com>
2020-08-20 16:00:31 +0530

    practice codes

--- practice.cpp
+++ practice.cpp
@@ -1,15 +1,17 @@
 #include <iostream>
-int cuberoot(int base);
+void getMax(int a,int b);
 
 using namespace std;
 
 int main(){
-    cuberoot(5);
+    getMax(5,2);
     return 0;
 }
 
-int cuberoot(int base){
-    int cuberoot = base * base * base;
-    cout << "the cube root of " << base << " is " << cuberoot << endl;
-    return cuberoot;
+void getMax(int a,int b){
+    if(a < b){
+        cout<<"b is greater"<<endl;
+    }else{
+        cout<<"a is greater"<<endl;
+    }
 }
