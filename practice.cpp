d1d5e6ddf43bdc5a0a0fb26356d5f3cfda368508
PriyadharshanRavi <mailmefriends95@gmail.com>
2020-08-25 15:59:12 +0530

    adding more practice codes

--- practice.cpp
+++ practice.cpp
@@ -1,17 +1,14 @@
 #include <iostream>
-void getMax(int a,int b);
 
 using namespace std;
 
 int main(){
-    getMax(5,2);
+    int x = 10;
+    do
+    {
+        cout<<x<<endl;
+        x++;
+    } while (x<=50);
+    
     return 0;
-}
-
-void getMax(int a,int b){
-    if(a < b){
-        cout<<"b is greater"<<endl;
-    }else{
-        cout<<"a is greater"<<endl;
-    }
 }
