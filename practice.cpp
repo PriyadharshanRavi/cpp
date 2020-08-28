da758486eaa17c8d4ca6980db30ddcbdcd22cf04
PriyadharshanRavi <mailmefriends95@gmail.com>
2020-08-28 13:50:46 +0530

    practice

--- practice.cpp
+++ practice.cpp
@@ -1,40 +1,26 @@
+//exponential function
+
 #include <iostream>
+int expo(int base, int power);
 
 using namespace std;
 
 int main(){
-    int secretNum = 8;
-    int guess;
-    int guessCount = 0;
-    int guessLimit = 3;
-    bool outOfGuesses=false;
-
-    while (secretNum != guess && !outOfGuesses)
-    {   
-        if (guessCount < guessLimit){
-            cout<<"enter your guess: ";
-            cin>>guess;
-            guessCount++;
-            cout<<"you have " << guessLimit - guessCount <<" guesses left" <<endl;
-            if (guessCount == 1 && secretNum != guess)
-            {
-                cout<<"Hint 1: The number is below 100" << endl;
-            }else if(guessCount == 2 && secretNum != guess){
-                cout<<"Hint 2: The number is below 50" << endl;
-            }else if(secretNum != guess){
-                cout<<"The number is " << secretNum << endl; 
-            }    
-        }else
-        {
-            outOfGuesses = true;
-        }
-    }
-    if (outOfGuesses)
-    {
-        cout<<"YOU LOST!!!"<<endl;
-    }else
-    {
-        cout << "**************YOU WIN!***************" <<endl;   
-    }
+    expo(5,5);
     return 0;
+}
+
+int expo(int base, int power){
+    cout << "enter the base number: ";
+    cin >> base;
+    cout << "enter the power number: ";
+    cin >> power;
+    int result = 1;
+    for (int i=0; i < power; i++)
+    {
+        result = result * base;
+    }
+    cout << "The answer is: " << result<<endl;
+    return result;
+    
 }
